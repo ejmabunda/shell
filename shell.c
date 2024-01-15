97c683aa7a00044a309e8aa487cf87a494888149
Junior <mjmabunda0@gmail.com>
2024-01-15 07:39:32 +0200

    Betty

--- shell.c
+++ shell.c
@@ -2,13 +2,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-#include <stdlib.h>
-
 #include "main.h"
 
 /**
  * shell - simple shell
- *
  * Return: this function does not return any value
  */
 void shell(void)
@@ -20,23 +17,16 @@ void shell(void)
 	char *_environ[1];
 
 	len = 0;
-
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			_puts("($) ");
-
 		read = getline(&line, &len, stdin);
 		if (read == -1)
 			break;
-
-		/* remove newline character at the end */
 		rm_newline(line);
-
 		argv[0] = line;
 		argv[1] = NULL;
-
-		/* try to execute command in a child process */
 		if (fork() == 0)
 		{
 			_environ[0] = NULL;
@@ -50,7 +40,6 @@ void shell(void)
 			wait(NULL);
 		}
 	}
-
 	free(line);
 	if (isatty(STDIN_FILENO))
 		_puts("\n");
