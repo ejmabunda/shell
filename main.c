05d4f3fe93454e6a1f3ee9c2b510a71007ed6038
Junior Mabunda <mjmabunda0@gmail.com>
2024-03-12 09:09:52 +0200

    [UPDATE] Update shell to execute first command

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "shell.h"
 
@@ -10,6 +13,8 @@ int main(void)
 	size_t len = 0;
 	ssize_t read;
 	char *token;
+	pid_t child_pid;
+	char *args[2];
 
 	while (1)
 	{
@@ -19,7 +24,7 @@ int main(void)
 		{
 			if (feof(stdin)) /* End of file */
 			{
-				_puts("\n");
+				_puts("\nGoodbye!\n");
 				exit(EXIT_SUCCESS);
 			}
 			else /* Error */
@@ -28,13 +33,43 @@ int main(void)
 				exit(EXIT_FAILURE);
 			}
 		}
+		
 		/* Tokenize the input line */
 		token = strtok(line, " \t\n");
-		
-		if (line[0] != '\n')
+		if (token != NULL)
 		{
-			_puts(line);
-			_puts(": No such file or directory\n");
+			/* Check if the token corresponds to a file */
+			if (access(token, X_OK) == 0)
+			{
+				child_pid = fork();
+				if (child_pid == -1)
+				{
+					perror("fork");
+					exit(EXIT_FAILURE);
+				}
+				else if (child_pid == 0) /* Child process */
+				{
+					/* Execute the command in the child process */
+					args[0] = token;
+					args[1] = NULL;
+					execve(token, args, NULL);
+					/* execve failed */
+					_puts("Command not found: ");
+					_puts(token);
+					_puts("\n");
+					exit(EXIT_FAILURE);
+				}
+				else /* Parent process */
+				{
+					/* Wait for the child process to complete */
+					wait(NULL);
+				}
+			}
+		        else
+			{
+				_puts(token);
+				_puts(": No such file or directory\n");
+			}
 		}
 	}
 	free(line);
