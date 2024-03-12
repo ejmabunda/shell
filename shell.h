f233f77a50fbb1566e85d13a99005fcce521dab1
Junior Mabunda <mjmabunda0@gmail.com>
2024-03-12 18:09:44 +0200

    [UPDATE] Betty

--- shell.h
+++ shell.h
@@ -1,10 +1,6 @@
-/* shell.h */
 #ifndef SHELL_H
 #define SHELL_H
 
-/* Include necessary standard libraries */
-
-/* Function prototypes */
 /* Returns the length of a string */
 int _strlen(const char *str);
 
@@ -17,4 +13,4 @@ int _strcmp(const char *str1, const char *str2);
 /* Tokenizes the input line, and tries to execute the first command. */
 void tokenize(char *line);
 
-#endif /* SHELL_H */
+#endif
